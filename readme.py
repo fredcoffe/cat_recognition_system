690506862e076ab84da3872c808d907c93ccb27f
fredcoffe <1660618066@qq.com>
2024-11-12 12:44:52 +0800

    Update readme.py

--- readme.py
+++ readme.py
@@ -6,6 +6,8 @@
 This code is for reporting and display purposes only and is prohibited.
 
 文件路径需要全英文
+识别大模型因大小问题还未上传，使用的https://github.com/xinyu1205/recognize-anything这个项目的模型
+代码中的路径为绝对路径，使用记得改为相对路径
 
 其他问题，请联系：18522830308
 
